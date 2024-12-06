541835b18c98d084aa83eab77b60befe78ed73db
Luide Mendes Rios <luidemendesrios@gmail.com>
2024-12-06 10:43:36 -0400

    Registro de novo usuário com javascript

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyAD5ZShUWaJb6opHELOikYRjpvQtahGSwY",
+        authDomain: "controle-de-gastos-4c953.firebaseapp.com",
+        projectId: "controle-de-gastos-4c953",
+        storageBucket: "controle-de-gastos-4c953.firebasestorage.app",
+        messagingSenderId: "728892738526",
+        appId: "1:728892738526:web:2af652e7eaac2d7643eb4f"
+      };
+      firebase.initializeApp(firebaseConfig);
